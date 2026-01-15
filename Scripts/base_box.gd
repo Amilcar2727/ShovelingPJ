ee04a466a87c83219309b13e8c44f001018aa596
Amilcar <bambino.amil.27@gmail.com>
2026-01-14 20:05:40 -0500

    Convertido box en clase y usando herencia

---
+++ Scripts/base_box.gd
@@ -0,0 +1,64 @@
+extends RigidBody2D
+class_name BaseBox
+
+var speed = 100;
+var hitted = false;
+var last_hitter = 0;
+var actual_modulate = "normal";
+var typeName = "Box";
+var ultima_velocidad:Vector2;
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	add_to_group("box");
+	_play_random_animation();
+	_fix_orientation();
+	_setup_physics();
+	actual_modulate = "normal";
+
+func _process(_delta):
+	if not hitted:
+		linear_velocity = Vector2(-speed, 0);
+		
+#Obtenemos la ultima velocidad pre impacto
+func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
+	ultima_velocidad = state.get_linear_velocity();
+	
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free();
+
+func _on_body_entered(_body):
+	hitted = true;
+	
+## Helpers
+func _play_random_animation():
+	var anims = $AnimatedSprite2D.sprite_frames.get_animation_names();
+	$AnimatedSprite2D.play(anims[randi()%anims.size()]);
+
+func _fix_orientation():
+	if(position.y<300):
+		$AnimatedSprite2D.scale.y = -0.08;
+		
+func _setup_physics():
+	contact_monitor = true;
+	max_contacts_reported = 1;
+	connect("body_entered",Callable(self,"_on_body_entered"));
+
+## Colores
+func ChangeColor(player_id):
+	if player_id == 1:
+		ChangeColorRed();
+	elif player_id == 2:
+		ChangeColorBlue();
+	else:
+		ChangeColorOrig();
+
+func ChangeColorOrig():
+	$AnimatedSprite2D.modulate = Color(1,1,1,1);
+	actual_modulate = "normal";
+func ChangeColorRed():
+	$AnimatedSprite2D.modulate = Color(0.75, 0, 0, 1);
+	actual_modulate = "red";
+func ChangeColorBlue():
+	$AnimatedSprite2D.modulate = Color(0, 0, 0.75, 1);
+	actual_modulate = "blue";
+	
--- Scripts/garbage.gd
+++ Scripts/garbage.gd
@@ -1,41 +1,12 @@
-extends RigidBody2D
+extends BaseBox
 
-var speed = 100;
-var hitted = false;
-var hitted_by_box = false;
-var last_hitter = 0;
-var actual_modulate = "";
-var typeName = "Garbage";
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	add_to_group("box");
-	var box_types = $AnimatedSprite2D.sprite_frames.get_animation_names();
-	$AnimatedSprite2D.play(box_types[randi()%box_types.size()]);
-	if(position.y<300):
-		$AnimatedSprite2D.scale.y = -0.08;
-	contact_monitor = true;
-	max_contacts_reported = 1;
-	connect("body_entered",Callable(self,"_on_body_entered"));
-	actual_modulate = "normal";
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	if hitted:
-		pass;
-	else:
-		linear_velocity = Vector2(-speed, 0);
-
-func _on_visible_on_screen_notifier_2d_screen_exited():
-	queue_free();
+	typeName = "Garbage"
+	super()
 	
-func ChangeColorOrig():
-	$AnimatedSprite2D.modulate = Color(1,1,1,1);
-	actual_modulate = "normal";
 func ChangeColorRed():
 	$AnimatedSprite2D.modulate = Color(1, 0.1, 0.1, 1);
 	actual_modulate = "red";
 func ChangeColorBlue():
 	$AnimatedSprite2D.modulate = Color(0.1, 0.35, 1, 1);
 	actual_modulate = "blue";
-	
-func _on_body_entered(_body):
-	hitted = true;
